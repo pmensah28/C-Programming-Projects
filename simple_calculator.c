8fb8855723998bdab5eddab76a551d63fb88061a
Prince Mensah <princemensaheyram@gmail.com>
2024-01-08 17:49:58 +0000

    Modified the code to include more functionalities using do while loop.

--- simple_calculator.c
+++ simple_calculator.c
@@ -9,40 +9,54 @@ like addtion, subtraction, multiplication and division.
 int main(){
     double num1, num2;
     char operator;
+    char quit;
 
-    printf("Enter your first number: \n");
-    scanf(" %lf", &num1);
+    do{
+        printf("\nEnter your first number: ");
+        scanf(" %lf", &num1);
+        
+        printf("\nEnter your prefered operaor ==> (+, -, *, /): ");
+        scanf(" %c", &operator);
+        
+        printf("\nEnter your second number: ");
+        scanf(" %lf", &num2);
+
+        switch (operator)
+        {
+        case '+' : 
+            printf("%.4lf + %.4lf = %.4lf \n", num1, num2, num1 + num2);
+            break;
+
+        case '-' : 
+            printf("%.4lf - %.4lf = %.4lf \n", num1, num2, num1-num2);
+            break;
+        
+        case '*' : 
+            printf("%.4lf * %.4lf = %.4lf \n", num1, num2, num1 * num2);
+            break;   
+
+        case '/' : 
+            if (num2 != 0)
+                printf("%.4lf / %.4lf = %.4lf \n", num1, num2, num1 / num2);
+            else
+                printf("Error: Division by zero! \n");
+            break;
+
+        default:
+            printf("Invalid Operation! Try again. \n");
+            break;
+        }
+
+        printf("\nDo you want to perform another calculation? (y/n): ");
+        scanf(" %c", &quit);
+
+            if(quit == 'y'){
+                printf("\nGreat...\n");
+            }else
+                printf("\nDone!\n");
     
-    printf("Enter your prefered operaor ==> (+, -, *, /) \n");
-    scanf(" %c", &operator);
+    }while(quit != 'n');
     
-    printf("Enter your second number: \n");
-    scanf(" %lf", &num2);
-
-    switch (operator)
-    {
-    case '+' : 
-        printf("%.4lf + %.4lf = %.4lf", num1, num2, num1 + num2);
-        break;
-
-    case '-' : 
-        printf("%.4lf - %.4lf = %.4lf", num1, num2, num1-num2);
-        break;
-    
-     case '*' : 
-        printf("%.4lf * %.4lf = %.4lf", num1, num2, num1 * num2);
-        break;   
-
-    case '/' : 
-        if (num2 != 0)
-            printf("%.4lf / %.4lf = %.4lf", num1, num2, num1 / num2);
-        else
-            printf("Error: Division by zero! \n");
-        break;
-
-    default: printf("Invalid Operation! Try again. \n");
-        break;
-    }
 
     return 0;
 }
